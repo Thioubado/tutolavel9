d3a08db148fa5223338f2ee109b84f53896b4d4a
Lionel COTE <grcote7@gmail.com>
2023-02-01 00:06:01 +0100

    Feat: Get only even values in an array (For Momo)

--- app/Http/Controllers/Gc7TestController.php
+++ app/Http/Controllers/Gc7TestController.php
@@ -1,10 +1,23 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Http\Tools\Gc7;
 use Barryvdh\Debugbar\Facades\Debugbar as DebugbarGc7;
 
 class Gc7TestController extends Controller {
 	public function test() {
+		$arr = range(1, 10);
+
+		Gc7::aff($arr);
+
+		foreach ($arr as $n) {
+			echo (!($n&1)) ? $n : '<br>';
+		}
+
+		// Rien ne t'empêche de faire ce genre de tableau, pour ensuite test la valeur et n'afficher que les pairs ;-)...
+
+		// Ici ton code pour ne filter dans ce tableau que les pairs
+
 		$data = 'A data from Gc7TestController<br>See In messages in the Debugbar';
 
 		DebugbarGc7::info('Info');
