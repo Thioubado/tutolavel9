faff792781ad2e56eb2f8ab0a6e75e30dbb33195
Lionel COTE <grcote7@gmail.com>
2023-02-03 08:47:42 +0100

    Fix: Indice pour Momo :-)

--- app/Http/Controllers/ShowListController.php
+++ app/Http/Controllers/ShowListController.php
@@ -1,38 +1,33 @@
 <?php
-
 namespace App\Http\Controllers;
 
 // call the model
+use App\Http\Tools\Gc7;
 use App\Models\Membre;
-use Illuminate\Http\Request;
 
-class ShowListController extends Controller
-{
-    //
-    public function getdata()
-    {
-         $data = Membre::orderBy('pseudo')->get();
-        
-        // $data = Membre::pluck('id')->get();
-        
-         return view('pages.show', ['membres' => $data]);
+class ShowListController extends Controller {
+	public function getdata() {
+		$data = Membre::orderBy('pseudo')->get();
 
+		// $data = Membre::pluck('id')->get();
 
-    }
-
-    public function pairs()
-	{
-		$data = [... Membre::pluck('id')];
-		//$data = Membre::orderBy('id')->get('id');
-		foreach ($data as $n) {
-			while ($n % 2 == 0) {
-				echo $n++.'<br>';
-			}
-		}
-		$n++;
-        return view('pages.show', ['membres' => $data]);
+		return view('pages.show', ['membres' => $data]);
 	}
 
+	public function pairs() {
+		$data = [...Membre::pluck('id')];
+		// $data = Membre::orderBy('id')->get('id');
+
+
+		foreach ($data as $n) {
+            while (0 == $n % 2) {
+                echo $n++ . '<br>';
+			}
+		}
+		++$n;
+
+        Gc7::aff($data, '$data');
+
+		// return view('pages.show', ['membres' => $data]);
+	}
 }
-
-
